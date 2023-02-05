ab94cddbcb072c394fada2e2b8bd96f0e4ae1e12
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-02-06 01:04:14 +0530

    Update

--- Final Project/MicroFinance/src/lk/ijse/microfinance/controller/GuaranteeItemController.java
+++ Final Project/MicroFinance/src/lk/ijse/microfinance/controller/GuaranteeItemController.java
@@ -1,25 +1,30 @@
 package lk.ijse.microfinance.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.microfinance.bo.BOFactory;
+import lk.ijse.microfinance.bo.custom.GuaranteeItemBO;
 import lk.ijse.microfinance.db.DBConnection;
 import lk.ijse.microfinance.model.GuaranteeItemModel;
+import lk.ijse.microfinance.to.DebtorDTO;
 import lk.ijse.microfinance.to.GuaranteeItemDTO;
+import lk.ijse.microfinance.to.GuarantorDTO;
+import lk.ijse.microfinance.view.tm.DebtorAddTm;
 import lk.ijse.microfinance.view.tm.GuaranteeItemAddTm;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,11 +45,15 @@ public class GuaranteeItemController {
     public Label lblLoanId;
     public Label lblName;
     public Label lblValivation;
+    public JFXButton btnRegisterID;
+    public JFXButton btnDeleteID;
+    public JFXButton btnNewRegisterID;
     private String searchText ="";
     private Matcher gItemIdMatcher;
     private Matcher gILoanIdMatcher;
     private Matcher gINameMatcher;
     private Matcher gIValivationMatcher;
+    GuaranteeItemBO guaranteeItemBO = (GuaranteeItemBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.GUARANTEEITEMBO);
 
 
     public void initialize(){
@@ -53,142 +62,167 @@ public class GuaranteeItemController {
         collName.setCellValueFactory(new PropertyValueFactory<>("name"));
         collValivation.setCellValueFactory(new PropertyValueFactory<>("valivation"));
 
+        initUI();
+        tblGuaranteeItem.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            btnDeleteID.setDisable(newValue==null);
+            btnRegisterID.setText(newValue!=null?"update":"save");
+            btnRegisterID.setDisable(newValue==null);
 
-        AddGuaranteeItemTbl(searchText);
-        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
-            searchText=newValue;
-            AddGuaranteeItemTbl(searchText);
+            if(newValue!=null){
+                txtItemID.setText(newValue.getId());
+                txtLoanID.setText(newValue.getLoanId());
+                txtName.setText(newValue.getName());
+                txtValivation.setText(String.valueOf(newValue.getValivation()));
+
+
+                txtItemID.setDisable(false);
+                txtLoanID.setDisable(false);
+                txtName.setDisable(false);
+                txtValivation.setDisable(false);
+
+            }
         });
+
+        txtItemID.setOnAction(event -> btnRegisterID.fire());
+        loadAllGItem();
+
+
+
+    }
+    public void initUI(){
+        txtItemID.clear();
+        txtLoanID.clear();
+        txtName.clear();
+        txtValivation.clear();
+
+        txtItemID.setDisable(true);
+        txtLoanID.setDisable(true);
+        txtName.setDisable(true);
+        txtValivation.setDisable(true);
+
+        txtItemID.setEditable(false);
+        btnRegisterID.setDisable(true);
+        btnDeleteID.setDisable(true);
 
     }
 
-    private void AddGuaranteeItemTbl(String text){
-        String searchText = "%"+text+"%";
-        try {
-            ObservableList<GuaranteeItemAddTm> tmList = FXCollections.observableArrayList();
-
-            Connection connection = DBConnection.getInstance().getConnection();
-            String sql = "SELECT * FROM GuaranteeItem WHERE gItemID LIKE ? || name LIKE ?";
-            PreparedStatement statement =connection.prepareStatement(sql);
-            statement.setString(1,searchText);
-            statement.setString(2,searchText);
-            ResultSet set = statement.executeQuery();
-            while(set.next()){
-                GuaranteeItemAddTm guaranteeItemAddTm = new GuaranteeItemAddTm(
-                        set.getString(1),
-                        set.getString(2),
-                        set.getString(3),
-                        set.getDouble(4));
-                tmList.add(guaranteeItemAddTm);
+    private void loadAllGItem(){
+        try{
+            guaranteeItemBO.gelAllGItem();
+            ArrayList<GuaranteeItemDTO> allGItem =  guaranteeItemBO.gelAllGItem();
+            for(GuaranteeItemDTO dto : allGItem){
+                tblGuaranteeItem.getItems().add(new GuaranteeItemAddTm(dto.getgItemID(),dto.getLoanId(),dto.getName(),dto.getValivation()));
             }
 
-            tblGuaranteeItem.setItems(tmList);
+        }catch (SQLException e){
 
-        }catch (ClassNotFoundException | SQLException e){
-
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
     public void btnRegisterOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-       GuaranteeItemDTO guaranteeItem  = new GuaranteeItemDTO(
-               txtItemID.getText(),
-               txtLoanID.getText(),
-               txtName.getText(),
-               Double.parseDouble(txtValivation.getText()));
-        boolean isAdd = GuaranteeItemModel.save(txtItemID.getText(),txtLoanID.getText());
+        String gItemID = txtItemID.getId();
+        String lID = txtLoanID.getText();
+        String name = txtName.getText();
+        double valivation  = Double.parseDouble(txtValivation.getText());
 
+        if (!name.matches(".*[a-zA-Z0-9]{4,}")) {
+            new Alert(Alert.AlertType.ERROR, "Invalid name").show();
+            txtName.requestFocus();
+//            txtName.setFocusColor(Paint.valueOf("Red"));
+            return;
+        } else if(!txtValivation.getText().matches(".*[0-9]{4,}")){
+            new Alert(Alert.AlertType.ERROR, "Valivation should not ").show();
+            txtValivation.requestFocus();
+        }
+        if(btnRegisterID.getText().equalsIgnoreCase("Save GuaranteeItem")){
+            try {
+                if(exitRegister(gItemID)){
+                    new Alert(Alert.AlertType.ERROR,gItemID+"Allready Register").show();
 
-        if(gItemIdMatcher.matches()) {
-            if(gILoanIdMatcher.matches()) {
-                if(gINameMatcher.matches()) {
-                    if(gIValivationMatcher.matches()) {
-
-
-                    } else {
-                        txtValivation.requestFocus();
-                        lblValivation.setText("invalid Contact ");
-                    }
-                } else {
-                    txtName.requestFocus();
-                    lblName.setText("invalid Address ");
                 }
-            } else {
-                txtLoanID.requestFocus();
-                lblLoanId.setText("invalid Name ");
+                guaranteeItemBO.addGItem(new GuaranteeItemDTO(gItemID,lID,name,valivation));
+                tblGuaranteeItem.getItems().add(new GuaranteeItemAddTm(gItemID,lID,name,valivation));
 
             }
-        } else {
-            txtItemID.requestFocus();
-            lblItemID.setText("invalid ID ");
-        }
-
-        if(isAdd){
-            System.out.println("add");
-        }
-        try{
-            boolean isAdded = GuaranteeItemModel.register(guaranteeItem);
-            AddGuaranteeItemTbl(searchText);
-            if(isAdded){
-                new Alert(Alert.AlertType.CONFIRMATION,"Guarantee Item Added!").show();;
-            }else {
-                new Alert(Alert.AlertType.WARNING,"Something Happened!").show();
+            catch (SQLException e){
+                new Alert(Alert.AlertType.ERROR,gItemID+"Failed").show();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        }catch (SQLException | ClassNotFoundException e){
-            throw new RuntimeException(e);
+        }else {
+            try{
+                if(!exitRegister(gItemID)){
+                    new Alert(Alert.AlertType.ERROR,gItemID+"Allready Update").show();
+                }
+                guaranteeItemBO.updateGItem(new GuaranteeItemDTO(gItemID,lID,name,valivation));
+
+                GuaranteeItemAddTm guaranteeItemAddTm = tblGuaranteeItem.getSelectionModel().getSelectedItem();
+                guaranteeItemAddTm.setLoanId(lID);
+                guaranteeItemAddTm.setName(name);
+                guaranteeItemAddTm.setValivation(valivation);
+
+                tblGuaranteeItem.refresh();
+
+            }catch (SQLException e){} catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                new Alert(Alert.AlertType.ERROR,gItemID+"Failed").show();
+                e.printStackTrace();
+            }
+
         }
+        btnNewRegisterID.fire();
+
+
+
     }
-
+    private boolean exitRegister(String code) throws SQLException, ClassNotFoundException {
+        return guaranteeItemBO.existGItem(code);
+    }
     public void btnUpdateOnAction(ActionEvent actionEvent) {
-        String gIid=txtItemID.getText();
-        String loanId=txtLoanID.getText();
-        String gIName=txtName.getText();
-        Double gIValivation=Double.parseDouble(txtValivation.getText());
+        txtItemID.setDisable(false);
+        txtLoanID.setDisable(false);
+        txtName.setDisable(false);
+        txtValivation.setDisable(false);
 
+        txtItemID.clear();
+        txtLoanID.clear();
+        txtName.clear();
+        txtValivation.clear();
 
-        String sql="UPDATE GuaranteeItem SET  lID = ?, name = ?, valivation = ? where gItemID = ?";
+        txtItemID.setText(genarateNewID());
+        btnRegisterID.setDisable(false);
+        btnRegisterID.setText("Save GuaranteeItem");
 
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pre=connection.prepareStatement(sql);
-            pre.setObject(1, txtLoanID.getText());
-            pre.setObject(2, txtName.getText());
-            pre.setObject(3, txtValivation.getText());
-            pre.setObject(4, txtItemID.getText());
-
-
-
-            int executeUpdate = pre.executeUpdate();
-            AddGuaranteeItemTbl(searchText);
-
-            if(executeUpdate>0){
-                new Alert(Alert.AlertType.CONFIRMATION,"Guarantor Updated!").show();;
-            }else {
-                new Alert(Alert.AlertType.WARNING,"Something Happened!").show();
-            }
-
-        } catch (Exception e) {
-
-        }
     }
+    private String genarateNewID() {
+        try{
+            return guaranteeItemBO.genaRateNewId();
+        }catch (SQLException e){
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "GI00-001";
+
+    }
     public void btnDeleteOnAction(ActionEvent actionEvent) {
-        String iID=txtItemID.getText();
+        String id = tblGuaranteeItem.getSelectionModel().getSelectedItem().getId();
 
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure DELETE ?", ButtonType.YES,ButtonType.NO);
+        Optional<ButtonType> buttonType = alert.showAndWait();
+        if (buttonType.get() == ButtonType.YES) {
+            try {
+                guaranteeItemBO.deleteGItem(id);
+                tblGuaranteeItem.getItems().remove(tblGuaranteeItem.getSelectionModel().getSelectedItem());
+                tblGuaranteeItem.getSelectionModel().clearSelection();
+                initUI();
+            }catch (SQLException e){
 
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            String sql="DELETE FROM GuaranteeItem WHERE gItemID=?";
-            PreparedStatement pre=connection.prepareStatement(sql);
-            pre.setObject(1, txtItemID.getText());
-            int executeUpdate = pre.executeUpdate();
-
-            if(executeUpdate>0){
-                new Alert(Alert.AlertType.CONFIRMATION," Deleted!").show();;
-
-                //JOptionPane.showMessageDialog(rootPane, "Delete Items");
-                AddGuaranteeItemTbl(searchText);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
         }
     }
 
@@ -196,47 +230,47 @@ public class GuaranteeItemController {
     public void txtLoanIdKeyTypeOnAction(KeyEvent keyEvent) {
         lblLoanId.setText("");
 
-        Pattern userIdPattern = Pattern.compile("^(E0)([0-9]{1})([0-9]{1,})$");
+        Pattern userIdPattern = Pattern.compile("");
         gILoanIdMatcher = userIdPattern.matcher(txtItemID.getText());
-
+/*
         if (!gILoanIdMatcher.matches()) {
             txtLoanID.requestFocus();
-            lblLoanId.setText("invalid ID");
-        }
+            lblLoanId.setText("invalid ID");*/
+        //}
     }
 
     public void txtItemIdKeyTypeOnAction(KeyEvent keyEvent) {
         lblItemID.setText("");
 
-        Pattern userIdPattern = Pattern.compile("^(GI0)([0-9]{1})([0-9]{1,})$");
+        Pattern userIdPattern = Pattern.compile("");
         gItemIdMatcher = userIdPattern.matcher(txtItemID.getText());
 
-        if (!gItemIdMatcher.matches()) {
+        /*if (!gItemIdMatcher.matches()) {
             txtItemID.requestFocus();
             lblItemID.setText("invalid ID");
-        }
+        }*/
     }
 
     public void txtNameKeyTypeOnAction(KeyEvent keyEvent) {
         lblName.setText("");
-        Pattern userNamePattern = Pattern.compile("^([a-zA-Z]{4,})$");
+        Pattern userNamePattern = Pattern.compile("");
         gINameMatcher = userNamePattern.matcher(txtName.getText());
 
-        if (!gItemIdMatcher.matches()) {
+       /* if (!gItemIdMatcher.matches()) {
             txtName.requestFocus();
             lblName.setText("invalid Name");
-        }
+        }*/
     }
 
     public void txtValivationKeyTypeOnAction(KeyEvent keyEvent) {
         lblValivation.setText("");
 
-        Pattern amountPattern = Pattern.compile("^[0-9]{1,}$");
+        Pattern amountPattern = Pattern.compile("");
         gIValivationMatcher = amountPattern.matcher(txtValivation.getText());
 
-        if (!gIValivationMatcher.matches()) {
+       /* if (!gIValivationMatcher.matches()) {
             txtValivation.requestFocus();
             lblValivation.setText("invalid Number");
-        }
+        }*/
     }
 }
