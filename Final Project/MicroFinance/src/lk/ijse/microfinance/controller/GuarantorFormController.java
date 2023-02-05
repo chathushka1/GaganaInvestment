f6811ad03234d58d5ddf0e0406823e08653d58bf
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-02-06 01:55:16 +0530

    Update

--- Final Project/MicroFinance/src/lk/ijse/microfinance/controller/GuarantorFormController.java
+++ Final Project/MicroFinance/src/lk/ijse/microfinance/controller/GuarantorFormController.java
@@ -1,5 +1,6 @@
 package lk.ijse.microfinance.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,15 +12,21 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.microfinance.bo.BOFactory;
+import lk.ijse.microfinance.bo.custom.GuarantorBO;
 import lk.ijse.microfinance.db.DBConnection;
+import lk.ijse.microfinance.entity.GuaranteeItem;
 import lk.ijse.microfinance.model.GuarantorModel;
+import lk.ijse.microfinance.to.DebtorDTO;
 import lk.ijse.microfinance.to.GuarantorDTO;
+import lk.ijse.microfinance.view.tm.DebtorAddTm;
 import lk.ijse.microfinance.view.tm.GuarantorAddTm;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,6 +52,9 @@ public class GuarantorFormController {
     public Label lblAddress;
     public Label lblNic;
     public Label lblTelephne;
+    public JFXButton btnNewRegisterID;
+    public JFXButton btnRegisterID;
+    public JFXButton btnDeleteID;
     private String searchText ="";
     private Matcher gIdMatcher;
     private Matcher gLoanIdMatcher;
@@ -52,8 +62,10 @@ public class GuarantorFormController {
     private Matcher gAddressMatcher;
     private Matcher gNicMatcher;
     private Matcher gTelephoneMatcher;
+    GuarantorBO guarantorBO = (GuarantorBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.GUARANTORBO);
 
     public void initialize(){
+
         collGuarantorID.setCellValueFactory(new PropertyValueFactory<>("id"));
         collLoanID.setCellValueFactory(new PropertyValueFactory<>("loanId"));
         collName.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -61,115 +73,56 @@ public class GuarantorFormController {
         collNIC.setCellValueFactory(new PropertyValueFactory<>("nic"));
         collTelephone.setCellValueFactory(new PropertyValueFactory<>("telephone"));
 
-        AddGuarantorTbl(searchText);
-        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
-            searchText=newValue;
-            AddGuarantorTbl(searchText);
-        });
-
-    }
-
-    public void setPattern(){
-        Pattern userIdPattern = Pattern.compile("^(G0)([0-9]{1})([0-9]{1,})$");
-        gIdMatcher = userIdPattern.matcher(txtGuarantorID.getText());
-
-        Pattern loanIDPattern = Pattern.compile("^(L0)([0-9]{1})([0-9]{1,})$");
-        gLoanIdMatcher = loanIDPattern.matcher(txtLoanID.getText());
-
-        Pattern userNamePattern = Pattern.compile("^([a-zA-Z]{4,})$");
-        gNameMatcher = userNamePattern.matcher(txtName.getText());
-
-        Pattern userAddressPattern = Pattern.compile("^([a-zA-Z]{4,})$");
-        gAddressMatcher = userAddressPattern.matcher(txtAddress.getText());
-
-        Pattern nicPattern = Pattern.compile("^[0-9]{10}[vVxX]$");
-        gNicMatcher = nicPattern.matcher(txtNIC.getText());
-
-        Pattern userContactPattern = Pattern.compile("^(?:7|0|(?:\\+94))[0-9]{9,10}$");
-        gTelephoneMatcher = userContactPattern.matcher(txtTelephone.getText());
 
 
     }
 
-    private void AddGuarantorTbl(String text){
-        String searchText = "%"+text+"%";
-        try {
-            ObservableList<GuarantorAddTm> tmList = FXCollections.observableArrayList();
+    public void initUI(){
+        /*private String gID ;
+        private String lID ;
+        private String name;
+        private String address;
+        private String nic ;
+        private String telephone ;
+*/
+        txtGuarantorID.clear();
+        txtLoanID.clear();
+        txtName.clear();
+        txtAddress.clear();
+        txtNIC.clear();
+        txtTelephone.clear();
 
-            Connection connection = DBConnection.getInstance().getConnection();
-            String sql = "SELECT * FROM Guarantor WHERE name LIKE ? || address LIKE ? || nic LIKE ?";
-            PreparedStatement statement =connection.prepareStatement(sql);
-            statement.setString(1,searchText);
-            statement.setString(2,searchText);
-            statement.setString(3,searchText);
-            ResultSet set = statement.executeQuery();
-            while(set.next()){
-                GuarantorAddTm guarantorAddTm = new GuarantorAddTm(set.getString(1),set.getString(2),set.getString(3),
-                        set.getString(4),set.getString(5),set.getString(6));
-                tmList.add(guarantorAddTm);
-            }
-           // System.out.println(tmList);
-            tblGuarantor.setItems(tmList);
+        txtGuarantorID.setDisable(true);
+        txtLoanID.setDisable(true);
+        txtName.setDisable(true);
+        txtAddress.setDisable(true);
+        txtNIC.setDisable(true);
+        txtTelephone.setDisable(true);
 
-        }catch (ClassNotFoundException | SQLException e){
+        txtGuarantorID.setEditable(false);
+        btnRegisterID.setDisable(true);
+        btnDeleteID.setDisable(true);
 
-        }
+
     }
-    public void btnRegisterOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-       GuarantorDTO guarantor = new GuarantorDTO(txtGuarantorID.getText(),txtLoanID.getText(),txtName.getText(),txtAddress.getText(),txtNIC.getText(),txtTelephone.getText());
-       // boolean isAdd = GuarantorRegisterModel.placeGuarantor(txtLoanID.getText(),txtGuarantorID.getText());
-
-        setPattern();
-        if(gIdMatcher.matches()) {
-            if (gLoanIdMatcher.matches()){
-                if(gNameMatcher.matches()) {
-                    if(gAddressMatcher.matches()) {
-                        if(gNicMatcher.matches()){
-                                if(gTelephoneMatcher.matches()) {
-
-                                } else {
-                                    txtTelephone.requestFocus();
-                                    lblTelephne.setText("invalid Contact ");
-                                }
-
-                        }else{
-                            txtNIC.requestFocus();
-                            lblNic.setText("invalid NIC");
-                        }
-
-                    } else {
-                        txtAddress.requestFocus();
-                        lblAddress.setText("invalid Address ");
-                    }
-                } else {
-                    txtName.requestFocus();
-                    lblName.setText("invalid Name ");
-                }
-            }else{
-                txtLoanID.requestFocus();
-                lblLoanId.setText("invalid ID ");
-            }
-        } else {
-            txtGuarantorID.requestFocus();
-            lblGuarantorId.setText("invalid ID ");
-
-        }
-
-        boolean isAdd = GuarantorModel.save(txtLoanID.getText(),txtGuarantorID.getText());
-        if(isAdd){
-            System.out.println("add");
-        }
+    private void loadAllGuarantor(){
         try{
-            boolean isAdded = GuarantorModel.register(guarantor);
-            AddGuarantorTbl(searchText);
-            if(isAdded){
-                new Alert(Alert.AlertType.CONFIRMATION,"Guarantor Added!").show();;
-            }else {
-                new Alert(Alert.AlertType.WARNING,"Something Happened!").show();
+            guarantorBO.gelAllGuarantor();
+            ArrayList<GuarantorDTO> allGuarantor = guarantorBO.gelAllGuarantor();
+            for(GuarantorDTO dto : allGuarantor){
+                tblGuarantor.getItems().add(new GuarantorAddTm(dto.getgID(),dto.getlID(),dto.getName(),dto.getAddress(),
+                        dto.getNic(),dto.getTelephone()));
             }
-        }catch (SQLException | ClassNotFoundException e){
-            throw new RuntimeException(e);
+
+        }catch (SQLException e){
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+    }
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+
     }
 
     public void btnUpdateOnAction(ActionEvent actionEvent) {
@@ -180,124 +133,139 @@ public class GuarantorFormController {
         String gNic=txtNIC.getText();
         String gTelephone=txtTelephone.getText();
 
-        String sql="UPDATE Guarantor SET lID = ?, name = ?, address = ?, nic = ?, telephone = ?  where gID = ?";
+        if (!gName.matches(".*[a-zA-Z0-9]{4,}")) {
+            new Alert(Alert.AlertType.ERROR, "Invalid name").show();
+            txtName.requestFocus();
+//            txtName.setFocusColor(Paint.valueOf("Red"));
+            return;
+        } else if (!gAddress.matches(".*[a-zA-Z0-9]{4,}")) {
+            new Alert(Alert.AlertType.ERROR, "Address should not ").show();
+            txtAddress.requestFocus();
+            return;
+        }else if(!gNic.matches(".*[a-zA-Z0-9]{4,}")){
+            new Alert(Alert.AlertType.ERROR, "NIC should not ").show();
+            txtNIC.requestFocus();
+        }else if (!gTelephone.matches(".*(?:7|0|(?:\\\\+94))[0-9]{9,10}")) {
+            new Alert(Alert.AlertType.ERROR, "Contact should be at long").show();
+            txtTelephone.requestFocus();
+            return;
+        }
+        if(btnRegisterID.getText().equalsIgnoreCase("Save Debtor")){
+            try {
+                if(exitRegister(gId)){
+                    new Alert(Alert.AlertType.ERROR,gId+"Allready Register").show();
 
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pre=connection.prepareStatement(sql);
+                }
+                guarantorBO.addGuarantor(new GuarantorDTO(gId,lId,gName,gAddress,gNic,gTelephone));
+                tblGuarantor.getItems().add(new GuarantorAddTm(gId,lId,gName,gAddress,gNic,gTelephone));
 
-            pre.setObject(1, txtLoanID.getText());
-            pre.setObject(2, txtName.getText());
-            pre.setObject(3, txtAddress.getText());
-            pre.setObject(4, txtNIC.getText());
-            pre.setObject(5, txtTelephone.getText());
-            pre.setObject(6, txtGuarantorID.getText());
+            }
+            catch (SQLException e){
+                new Alert(Alert.AlertType.ERROR,gId+"Failed").show();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else {
+            try{
+                if(!exitRegister(gId)){
+                    new Alert(Alert.AlertType.ERROR,gId+"Allready Update").show();
+                }
+                guarantorBO.updateGuarantor(new GuarantorDTO(gId,lId,gName,gAddress,gNic,gTelephone));
 
+                GuarantorAddTm guarantorAddTm = (GuarantorAddTm) tblGuarantor.getSelectionModel().getSelectedItem();
 
-            int executeUpdate = pre.executeUpdate();
-            AddGuarantorTbl(searchText);
+                guarantorAddTm.setLoanId(lId);
+                guarantorAddTm.setName(gName);
+                guarantorAddTm.setAddress(gAddress);
+                guarantorAddTm.setNic(gNic);
+                guarantorAddTm.setTelephone(gTelephone);
 
-            if(executeUpdate>0){
-                new Alert(Alert.AlertType.CONFIRMATION,"Guarantor Updated!").show();;
-            }else {
-                new Alert(Alert.AlertType.WARNING,"Something Happened!").show();
+            }catch (SQLException e){} catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                new Alert(Alert.AlertType.ERROR,gId+"Failed").show();
+                e.printStackTrace();
             }
 
-        } catch (Exception e) {
-
         }
+        btnNewRegisterID.fire();
+    }
+    private boolean exitRegister(String code) throws SQLException, ClassNotFoundException {
+        return guarantorBO.existGuarantor(code);
     }
 
     public void btnDeleteOnAction(ActionEvent actionEvent) {
-        String gID=txtGuarantorID.getText();
 
-
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            String sql="DELETE FROM Guarantor WHERE gID = ?";
-            PreparedStatement pre=connection.prepareStatement(sql);
-            pre.setObject(1, txtGuarantorID.getText());
-            int executeUpdate = pre.executeUpdate();
-
-            if(executeUpdate>0){
-                new Alert(Alert.AlertType.CONFIRMATION," Deleted!").show();;
-
-                //JOptionPane.showMessageDialog(rootPane, "Delete Items");
-                AddGuarantorTbl(searchText);
-            }
-        } catch (Exception e) {
-        }
     }
 
     public void txtgIdKeyTypeOnAction(KeyEvent keyEvent) {
         lblGuarantorId.setText("");
 
-        Pattern userIdPattern = Pattern.compile("^(G0)([0-9]{1})([0-9]{1,})$");
+        Pattern userIdPattern = Pattern.compile("");
         gIdMatcher = userIdPattern.matcher(txtGuarantorID.getText());
 
-        if (!gIdMatcher.matches()) {
+       /* if (!gIdMatcher.matches()) {
             txtGuarantorID.requestFocus();
             lblGuarantorId.setText("invalid ID");
-        }
+        }*/
     }
 
     public void txtgLoanIdKeyTypeOnAction(KeyEvent keyEvent) {
         lblLoanId.setText("");
 
-        Pattern loanIDPattern = Pattern.compile("^(L0)([0-9]{1})([0-9]{1,})$");
+        Pattern loanIDPattern = Pattern.compile("");
         gLoanIdMatcher = loanIDPattern.matcher(txtLoanID.getText());
 
-        if (!gLoanIdMatcher.matches()) {
+        /*if (!gLoanIdMatcher.matches()) {
             txtLoanID.requestFocus();
             lblLoanId.setText("invalid ID");
-        }
+        }*/
     }
 
     public void txtgNameKeyTypeOnAction(KeyEvent keyEvent) {
         lblName.setText("");
 
-        Pattern userNamePattern = Pattern.compile("^([a-zA-Z]{4,})$");
+        Pattern userNamePattern = Pattern.compile("");
         gNameMatcher = userNamePattern.matcher(txtName.getText());
-
+/*
         if (!gNameMatcher.matches()) {
             txtName.requestFocus();
             lblName.setText("invalid Name");
-        }
+        }*/
     }
 
     public void txtgAddressKeyTypeOnAction(KeyEvent keyEvent) {
         lblAddress.setText("");
 
-        Pattern userAddressPattern = Pattern.compile("^([a-zA-Z]{4,})$");
+        Pattern userAddressPattern = Pattern.compile("");
         gAddressMatcher = userAddressPattern.matcher(txtAddress.getText());
 
-        if (!gAddressMatcher.matches()) {
+        /*if (!gAddressMatcher.matches()) {
             txtAddress.requestFocus();
             lblAddress.setText("invalid Address");
-        }
+        }*/
     }
 
     public void txtgNicKeyTypeOnAction(KeyEvent keyEvent) {
         lblNic.setText("");
 
-        Pattern nicPattern = Pattern.compile("^[0-9]{10}[vVxX]$");
+        Pattern nicPattern = Pattern.compile("");
         gNicMatcher = nicPattern.matcher(txtNIC.getText());
 
-        if (!gNicMatcher.matches()) {
+       /* if (!gNicMatcher.matches()) {
             txtNIC.requestFocus();
             lblNic.setText("invalid Nic");
-        }
+        }*/
     }
 
     public void txtgTelephoneKeyTypeOnAction(KeyEvent keyEvent) {
         lblTelephne.setText("");
 
-        Pattern userContactPattern = Pattern.compile("^(?:7|0|(?:\\+94))[0-9]{9,10}$");
+        Pattern userContactPattern = Pattern.compile("");
         gTelephoneMatcher = userContactPattern.matcher(txtTelephone.getText());
 
-        if (!gTelephoneMatcher.matches()) {
+        /*if (!gTelephoneMatcher.matches()) {
             txtTelephone.requestFocus();
             lblTelephne.setText("invalid Number");
-        }
+        }*/
     }
 }
